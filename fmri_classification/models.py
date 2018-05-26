aee0d950b10753b16164ff65eb5cd911bec8363e
Harshvardhan Gazula <hvgazula@umich.edu>
2018-05-25 20:35:05 -0600

    Added X matrix

--- fmri_classification/models.py
+++ fmri_classification/models.py
@@ -4,23 +4,32 @@ Created on Fri May 25 13:57:49 2018
 
 @author: hgazula
 """
-import h5py
 import numpy as np
 import scipy.io as sio
+from polyssifier import poly
 
-# Loading X data
-f = h5py.File('data/MixingMatrix_Weights.mat')
-X = np.array(f["MixingMatrix_Weights"])
-print('Actual shape of X is: ', X.shape)
+## Loading X data
+#f = h5py.File('data/X.mat')
+#X = np.array(f["MixingMatrix_Weights"])
+#print('Actual shape of X is: ', X.shape)
+#
+## Reshaping the X matrix
+#X = np.reshape(X, (869, -1), order='C')
+#print('After reshaping :', X.shape)
 
-# Reshaping the X matrix
-X = np.reshape(X, (869, -1))
-print('After reshaping :', X.shape)
+# Loading y data
+X = sio.loadmat('data/X.mat')
+X = X['X']
+print('Shape of X is: ', X.shape)
 
 # Loading y data
 y = sio.loadmat('data/labels.mat')
 y = y['labels']
 print('Shape of y is: ', y.shape)
+y = np.reshape(y, (869,))
+
+# Polyssifier
+report = poly(X.T, y)
 
 # Logistic Regression
 
