02909844d4d31e1a9b2c9e07406acbe3b49f9f97
hvgazula <hvgazula@umich.edu>
2018-06-01 15:36:27 -0600

    Created a submission file for sfs

---
+++ fmri_classification/create_job_submission_sequential.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  1 15:30:53 2018
+
+@author: Harshvardhan
+"""
+import os
+import stat
+
+print('Creating job_scripts folder')
+jobs_folder = 'job_scripts'
+if not os.path.exists(jobs_folder):
+    os.makedirs(jobs_folder)
+
+max_runs = 10
+
+print('Writing job submission script files')
+for run_index in range(1, max_runs + 1):
+
+    file_name = 'run_{:03}.sh'.format(run_index)
+    with open(os.path.join(jobs_folder, file_name), 'w') as fsub:
+        fsub.write('echo \'Hello There!\'')
+        fsub.write('\n')
+        fsub.write('python sequential_selection.py {}'.format(
+            run_index))
+
+    st = os.stat(os.path.join(jobs_folder, file_name))
+    os.chmod(os.path.join(jobs_folder, file_name), st.st_mode | stat.S_IEXEC)
+
+    with open('job.sh', 'a') as fjob:
+        fjob.write('screen -dm bash ' + os.path.join(jobs_folder, file_name))
+        fjob.write('\n')
+
+print('Creating the main job.sh file')
+st = os.stat('job.sh')
+os.chmod('job.sh', st.st_mode | stat.S_IEXEC)
--- fmri_classification/sequential_selection.py
+++ fmri_classification/sequential_selection.py
@@ -5,49 +5,18 @@ Created on Wed May 30 09:50:07 2018
 
 @author: Harshvardhan
 """
+import os
 import pandas as pd
 import shelve
+import sys
 from load_data_from_mat import return_X_and_y
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC, SVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score, roc_auc_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklear.metrics import f1_score, precision_score, recall_score
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
-
-
-#def perform_sfs(main_model, X_train, X_test, y_train, y_test):
-#    sfs1 = SFS(
-#        logreg,
-#        k_features=5,
-#        verbose=1,
-#        forward=True,
-#        floating=False,
-#        scoring='accuracy',
-#        cv=0,
-#        n_jobs=-1)
-#
-#    sfs1 = sfs1.fit(X_train, y_train)
-#
-#    print('Selected features:', sfs1.k_feature_idx_)
-#
-#    # Generate the new subsets based on the selected features
-#    # Note that the transform call is equivalent to
-#    # X_train[:, sfs1.k_feature_idx_]
-#
-#    X_train_sfs = sfs1.transform(X_train)
-#    X_test_sfs = sfs1.transform(X_test)
-#
-#    # Fit the estimator using the new feature subset
-#    # and make a prediction on the test data
-#    main_model.fit(X_train_sfs, y_train)
-#    y_pred = main_model.predict(X_test_sfs)
-#
-#    # Compute the accuracy of the prediction
-#    acc = float((y_test == y_pred).sum()) / y_pred.shape[0]
-#    print('Test set accuracy: %.2f %%' % (acc * 100))
-#
-#    return sfs1
 
 
 def get_test_score(X_train, X_test, y_train, y_test, curr_feature_set,
@@ -60,51 +29,53 @@ def get_test_score(X_train, X_test, y_train, y_test, curr_feature_set,
     main_model.fit(X_train_sfs, y_train)
     y_pred = main_model.predict(X_test_sfs)
 
-    
     keys = {'accuracy', 'precision', 'recall', 'f1_score', 'confusion_matrix'}
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
     f1score = f1_score(y_test, y_pred)
     confusion = confusion_matrix(y_test, y_pred)
-    
+
     return dict(zip(keys, [accuracy, precision, recall, f1score, confusion]))
-    
+
+
+def perform_sfs(curr_classifier, X_train, X_test, y_train, y_test):
+    sfs1 = SFS(
+        curr_classifier,
+        k_features=5,
+        verbose=1,
+        forward=True,
+        floating=False,
+        scoring='accuracy',
+        cv=5,
+        n_jobs=-1)
+
+    sfs1 = sfs1.fit(X_train, y_train)
+    df = pd.DataFrame.from_dict(sfs1.get_metric_dict(), orient='index')
+    df[[
+        'accuracy', 'precision', 'recall', 'f1_score', 'confusion_matrix'
+    ]] = df['feature_idx'].apply(
+        lambda x: get_test_score(X_train, X_test, y_train, y_test, x, logreg)
+    ).apply(pd.Series)
+
+    return df
+
 
 if __name__ == '__main__':
+
     X, y = return_X_and_y()
 
-    ### start of whiteboard code
-    num_of_runs = 1
     max_features_selected = 5
 
-    for run in range(num_of_runs):
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.10)
+    run_number = int(sys.argv[1])
 
-        logreg = LogisticRegression()
+    file_name = 'Results_from_run_{:03}'.format(run_number)
 
-        sfs1 = SFS(
-            logreg,
-            k_features=5,
-            verbose=1,
-            forward=True,
-            floating=False,
-            scoring='accuracy',
-            cv=5,
-            n_jobs=-1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
 
-        sfs1 = sfs1.fit(X_train, y_train)
-        df = pd.DataFrame.from_dict(sfs1.get_metric_dict(), orient='index')
-        df['scores_dict'] = df['feature_idx'].apply(
-            lambda x: get_test_score(X_train, X_test, y_train, y_test, x, logreg)
-        )
+    logreg = LogisticRegression()
+    logreg_sfs = perform_sfs(logreg, X_train, X_test, y_train, y_test)
 
-### end of whiteboard code
-
-#    logreg = LogisticRegression()
-#    logreg_sfs = perform_sfs(logreg, X_train, X_test, y_train, y_test)
-#
 #    simple_svm = LinearSVC()
 #    simple_svm_sfs = perform_sfs(simple_svm, X_train, X_test, y_train, y_test)
 #
@@ -117,6 +88,7 @@ if __name__ == '__main__':
     print('Writing data to a shelve file')
     results = shelve.open(os.path.join('Results', file_name))
     results['logistic'] = logreg_sfs
-    results['linear_svm'] = simple_svm_sfs
-    results['radial_svm'] = radial_svm_sfs
+#    results['linear_svm'] = simple_svm_sfs
+#    results['radial_svm'] = radial_svm_sfs
+#    results['lda'] = lda_sfs
     results.close()
