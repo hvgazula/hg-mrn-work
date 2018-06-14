ccdf42aad32540fa9b85334e2c258cbf261b9a7b
Harshvardhan Gazula <hvgazula@umich.edu>
2018-06-14 17:24:26 -0600

    Modified load_data_from_mat.py script

--- fmri_classification/load_data_from_mat.py
+++ fmri_classification/load_data_from_mat.py
@@ -9,17 +9,18 @@ import numpy as np
 import scipy.io as sio
 
 
-def return_X_and_y():
-
-    # Loading X data
-    print('Loading X data')
-    X = sio.loadmat('data/X.mat')
-    X = X['X']
-
-    # Loading y data
-    print('Loading y data')
-    y = sio.loadmat('data/labels.mat')
-    y = y['labels']
+def return_X_and_y(folder_name):
+    """Loads the data relevant to the folder specified"""
+    if folder_name == 'Part_01':
+        X = sio.loadmat('data/X.mat')['X']  # Loading X data
+        y = sio.loadmat('data/labels.mat')['labels']  # Loading y data
+    elif folder_name == 'Part_02':
+        data = sio.loadmat('data/Part_02/FNC_residual.mat')
+        
+        X = data['residuals']  # Loading X data
+        y = data['diagnosis']  # Loading y data
+    else:
+        print('Folder not found.')
+        
     y = np.reshape(y, (869, ))
-
     return X, y
