2226a24df88d91959fdf9f2c5431ef5a2d845cf5
Harshvardhan Gazula <hvgazula@umich.edu>
2018-06-14 17:27:44 -0600

    some modifications

--- fmri_classification/load_data_from_mat.py
+++ fmri_classification/load_data_from_mat.py
@@ -6,21 +6,24 @@ Created on Wed May 30 12:02:23 2018
 @author: Harshvardhan
 """
 import numpy as np
+import os
 import scipy.io as sio
 
 
 def return_X_and_y(folder_name):
     """Loads the data relevant to the folder specified"""
     if folder_name == 'Part_01':
-        X = sio.loadmat('data/X.mat')['X']  # Loading X data
-        y = sio.loadmat('data/labels.mat')['labels']  # Loading y data
+        X = sio.loadmat(
+            os.path.join('data', folder_name, 'X.mat'))['X']  # Loading X data
+        y = sio.loadmat(os.path.join('data', folder_name, 'labels.mat'))[
+            'labels']  # Loading y data
     elif folder_name == 'Part_02':
-        data = sio.loadmat('data/Part_02/FNC_residual.mat')
-        
+        data = sio.loadmat(
+            os.path.join('data', folder_name, 'FNC_residual.mat'))
         X = data['residuals']  # Loading X data
         y = data['diagnosis']  # Loading y data
     else:
         print('Folder not found.')
-        
+
     y = np.reshape(y, (869, ))
     return X, y
