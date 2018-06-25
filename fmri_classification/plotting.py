7374a0194047de1f45f565432db0cc9bf2dbb7e5
hvgazula <hvgazula@umich.edu>
2018-06-25 07:22:42 -0600

    some cleanup

--- fmri_classification/plotting.py
+++ fmri_classification/plotting.py
@@ -14,93 +14,112 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.backends.backend_pdf
 
-sns.set()
 
-pdf = matplotlib.backends.backend_pdf.PdfPages("test.pdf")
+def extract_feature_idx_order(df):
+    main_list = []
+    for index, df in enumerate(df_list):
+        curr_series = df['feature_idx']
+        my_list = []
+        my_list.append(curr_series[1][0])
+        for i in range(1, len(curr_series)):
+            curr_elem = list(set(curr_series[i + 1]) - set(curr_series[i]))
+            my_list.append(curr_elem[0])
+        main_list.append(my_list)
 
-classifier_dict = {
-    'knn': 'K Nearest Neighbors (K = 5)',
-    'lda': 'Linear Discriminant Analysis',
-    'logistic': 'Logistic Regression',
-    'simple_svm': 'Linear SVM',
-    'radial_svm': 'RBF SVM',
-}
+    return main_list
 
-results_folder_list = list(
-    filter(os.path.isdir, glob.glob('Results*')))
 
-for folder in results_folder_list:
-    code = folder.split('_')[1]
-    data_set, features_selected, runs = code.split('-')
+if __name__ == '__main__':
 
-    for key in classifier_dict.keys():
-        try:
-            file_list = sorted([
-                os.path.splitext(x)[0]
-                for x in glob.glob(os.path.join(folder, key + '/*.dat'))
-            ])
+    sns.set()
 
-            df_list = [shelve.open(file)[key] for file in file_list]
+    output_file = 'test.pdf'
+    pdf = matplotlib.backends.backend_pdf.PdfPages(output_file)
 
-            main_list = []
-            for index, df in enumerate(df_list):
-                curr_series = df['feature_idx']
-                my_list = []
-                my_list.append(curr_series[1][0])
-                for i in range(1, len(curr_series)):
-                    curr_elem = list(
-                        set(curr_series[i + 1]) - set(curr_series[i]))
-                    my_list.append(curr_elem[0])
-                main_list.append(my_list)
+    classifier_dict = {
+        'knn': 'K Nearest Neighbors (K = 5)',
+        'lda': 'Linear Discriminant Analysis',
+        'logistic': 'Logistic Regression',
+        'simple_svm': 'Linear SVM',
+        'radial_svm': 'RBF SVM',
+    }
 
-            index_selection_order_fig = plt.figure()
-            index_df = pd.DataFrame(main_list)
-            for col in index_df.columns:
-                y = index_df[col]
-                x = np.ones(y.shape) * (col + 1)
-                plt.scatter(x, y, s=5)
-                plt.xlabel('Feature Selection Step')
-                plt.ylabel('Feature Index')
-                plt.title('{} (Dataset = {}, Features selected = {})'.format(
-                    classifier_dict[key], data_set, features_selected))
+    # Folder List
+    select_dir = os.path.join('/Users/Harshvardhan/Downloads', 'Results*')
+    dir_contents = glob.glob(select_dir)
+    results_folders = filter(os.path.isdir, dir_contents)
 
-            pdf.savefig(index_selection_order_fig)
-            plt.close(index_selection_order_fig)
+    for folder in results_folders:
+        results_code = folder.split('_')[1]
+        data_set, features_selected, runs = results_code.split('-')
 
-            # Print Performance Criterion Plots
-            performance_criterion = ['accuracy', 'f1_score']
-            for criterion in performance_criterion:
-                fig = plt.figure()
-                curr_df = [df[criterion] for df in df_list]
-                concat_df = pd.concat(curr_df, axis=1)
-                concat_df.columns = list(range(1, len(concat_df.columns) + 1))
+        for key in classifier_dict.keys():
+            try:
+                sub_directory = os.path.join(folder, key, '*.dat')
+                sub_dir_files = glob.glob(sub_directory)
 
-                for col in concat_df.columns:
-                    y = concat_df[col]
-                    x = np.ones(y.shape) * col
-                    plt.scatter(x, y)
-                    plt.xlabel('Run Index')
-                    plt.ylabel(criterion)
+                if not sub_dir_files:
+                    continue
+
+                files_without_ext = map(os.path.splitext, sub_dir_files)
+                file_list = sorted([x[0] for x in files_without_ext])
+
+                df_list = [shelve.open(file)[key] for file in file_list]
+
+                feature_idx_order = extract_feature_idx_order(df_list)
+
+                feature_idx_order_fig = plt.figure()
+                index_df = pd.DataFrame(feature_idx_order)
+                for col in index_df.columns:
+                    y = index_df[col]
+                    x = np.ones(y.shape) * (col + 1)
+                    plt.scatter(x, y, s=5)
+                    plt.xlabel('Feature Selection Step')
+                    plt.ylabel('Feature Index')
                     plt.title(
                         '{} (Dataset = {}, Features selected = {})'.format(
                             classifier_dict[key], data_set, features_selected))
 
-                pdf.savefig(fig)
-                plt.close(fig)
+                pdf.savefig(feature_idx_order_fig)
+                plt.close(feature_idx_order_fig)
 
-                # Print index at which maximum performance is achieved
-                index_fig = plt.figure()
-                y = concat_df.idxmax(axis=0)
-                x = np.arange(1, len(y) + 1)
-                plt.scatter(x, y)
-                plt.xlabel('Run Index')
-                plt.ylabel('Step Max ' + criterion + ' was achieved')
-                plt.title('{} (Dataset = {}, Features selected = {})'.format(
-                    classifier_dict[key], data_set, features_selected))
-                pdf.savefig(index_fig)
-                plt.close(index_fig)
+                # Print Performance Criterion Plots
+                performance_criterion = ['accuracy', 'f1_score']
+                for criterion in performance_criterion:
+                    fig = plt.figure()
+                    curr_df = [df[criterion] for df in df_list]
+                    concat_df = pd.concat(curr_df, axis=1)
+                    concat_df.columns = list(
+                        range(1, len(concat_df.columns) + 1))
 
-        except Exception:
-            pass
+                    for col in concat_df.columns:
+                        y = concat_df[col]
+                        x = np.ones(y.shape) * col
+                        plt.scatter(x, y)
+                        plt.xlabel('Run Index')
+                        plt.ylabel(criterion)
+                        plt.title('{} (Dataset = {}, Features selected = {})'.
+                                  format(classifier_dict[key], data_set,
+                                         features_selected))
 
-pdf.close()
+                    pdf.savefig(fig)
+                    plt.close(fig)
+
+                    # Print index at which maximum performance is achieved
+                    index_fig = plt.figure()
+                    y = concat_df.idxmax(axis=0)
+                    x = np.arange(1, len(y) + 1)
+                    plt.scatter(x, y)
+                    plt.xlabel('Run Index')
+                    plt.ylabel('Step Max ' + criterion + ' was achieved')
+                    plt.title(
+                        '{} (Dataset = {}, Features selected = {})'.format(
+                            classifier_dict[key], data_set, features_selected))
+                    pdf.savefig(index_fig)
+                    plt.close(index_fig)
+
+            except Exception as e:
+                print(e)
+                pass
+
+    pdf.close()
