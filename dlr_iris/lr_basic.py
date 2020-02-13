064985c39ee0d18cf2a533ba3dbe40645f085802
hvgazula <hvgazula@umich.edu>
2020-02-13 06:35:30 -0500

    Add lr_basic.py

--- dlr_iris/lr_basic.py
+++ dlr_iris/lr_basic.py
@@ -6,10 +6,10 @@ Created on Sun Feb  9 10:23:38 2020
 @author: hgazula
 """
 
-#LR from scratch
-
 from sklearn import datasets
 import numpy as np
+
+np.seterr(all='ignore')
 
 
 def sigmoid(z):
@@ -40,8 +40,8 @@ y = (iris.target != 0) * 1
 num_iter = 300000
 lr = 0.1
 
-theta = np.zeros(X.shape[1])
-
+# for loop
+theta = np.zeros((X.shape[1], 1))
 for i in range(num_iter):
     z = np.dot(X, theta)
     h = sigmoid(z)
@@ -49,45 +49,49 @@ for i in range(num_iter):
     prev_loss = loss(X, y, theta)
     theta -= lr * grad
     curr_loss = loss(X, y, theta)
-    print(abs(curr_loss-prev_loss))
+print(theta)
         
+# while loop
 theta = np.zeros(X.shape[1])
-while True:
-    prev_loss = loss(X, y, theta)
+i = 0
+while True and i < num_iter:
+    i = i + 1
     z = np.dot(X, theta)
     h = sigmoid(z)
     grad = np.dot(X.T, (h - y)) / y.size
+    prev_loss = loss(X, y, theta)
     theta -= lr * grad
     curr_loss = loss(X, y, theta)
     
     ldiff = abs(curr_loss-prev_loss)
-    print(ldiff)
+    # print(ldiff)
     if ldiff <= 1e-8 or np.isnan(ldiff):
         break
+print(theta)
     
+# adam
 beta1 = 0.9
 beta2 = 0.999
 eps = 1e-8
 mt, vt = 0, 0
 tol = 2e-8
-lr = 0.1
-
+lr = 0.01
+i = 0
 theta = np.zeros(X.shape[1])
-while True:
-    grad = gradient(X, y, theta)
+while True and i < num_iter:
+    i = i + 1
 
+    grad = gradient(X, y, theta)
     mt = beta1 * mt + (1 - beta1) * grad
     vt = beta2 * vt + (1 - beta2) * (grad**2)
-
     m = mt / (1 - beta1**i)
     v = vt / (1 - beta2**i)
-    print(mt, vt, m, v)
     prev_loss = loss(X, y, theta)
     theta = theta - lr * m / (np.sqrt(v) + eps)
-    curr_loss = loss(X, y, theta)
-
-    # print(prev_loss, curr_loss)
+    curr_loss = loss(X, y, theta) 
+    print(curr_loss)
+    
     ldiff = abs(curr_loss-prev_loss)
-    print(ldiff)
     if ldiff <= 1e-8 or np.isnan(ldiff):
         break
+print(theta)
